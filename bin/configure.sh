3da59870c178de6ab07472e69c4512d5e47777a6
Luis Capelo <luiscape@gmail.com>
2015-08-13 22:40:06 -0400

    deleting the configure bash script

--- bin/configure.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-#
-# Creates an user in the
-# MongoDB instance.
-#
-mongo $MONGODB_DATABASE --eval "db.createUser({user: '$MONGODB_USER_NAME', pwd: '$MONGODB_USER_PASSWORD', roles: [{ role: 'readWrite', db: '$MONGODB_DATABASE' },]})"
